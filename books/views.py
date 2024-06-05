1a419ba5d625d06ebfdd617a16d043d811b23885
CaueGomes07 <caue.gomes07@gmail.com>
2024-06-05 20:12:05 -0300

    projeto-pronto-sem-estilização-css

--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book
+from django.http import HttpResponse
+from django.template import loader
+#from .forms import RegistroForm
+
 
 # Create your views here.
+def bookList(request):
+    template = loader.get_template('book_list.html')
+    books = Book.objects.all()
+    context = {
+        'books': books,
+        'title': 'Estante Virtual'
+        }
+    return HttpResponse(template.render(context, request))
